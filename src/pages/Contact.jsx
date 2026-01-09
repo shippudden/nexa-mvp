4693a1f9e6beb01e267198a9242bc9b0b8c53fd4
boluwatife <boluwatifeda45@gmail.com>
2026-01-09 16:49:17 +0100

    new changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { agents } from "../data/agents"
 
 export default function Contact() {
